bd56ad8d5342949a084bf74c98c801cd2df9ac57
João Paulo Cercal <joao.cercal@a2c.com.br>
2014-04-29 11:11:38 -0300

    Internacionalização e customização de DataFixtures.

--- DataFixtures/ORM/DataFixtureLoader.php
+++ DataFixtures/ORM/DataFixtureLoader.php
@@ -2,6 +2,8 @@
 
 namespace Cekurte\PageBundle\DataFixtures\ORM;
 
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Cekurte\PageBundle\Entity\Page;
@@ -10,36 +12,46 @@ use Cekurte\PageBundle\Entity\PageWidgetType;
 use Cekurte\PageBundle\Entity\PageWidgetField;
 use Cekurte\PageBundle\Entity\PageWidgetCustomField;
 
-class DataFixtureLoader implements FixtureInterface
+class DataFixtureLoader implements FixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
     /**
      * {@inheritDoc}
      */
-    public function load(ObjectManager $manager)
+    public function setContainer(ContainerInterface $container = null)
     {
-        $pageWidgetTypeContact      = new PageWidgetType('Contact');
-        $pageWidgetTypeGoogleMap    = new PageWidgetType('Google Map');
+        $this->container = $container;
+    }
 
-        $manager->persist($pageWidgetTypeContact);
-        $manager->persist($pageWidgetTypeGoogleMap);
+    private function trans($message)
+    {
+        return $this->container->get('cekurte_page.translator')->trans($message);
+    }
 
-        $pageAbout      = new Page();
-        $pageContact    = new Page();
-
-        $pageAbout
-            ->setTitle('About')
-            ->setAbstract('')
-            ->setDescription('About our business.')
-        ;
+    /**
+     * Load Page Contact
+     *
+     * @param ObjectManager $manager
+     *
+     * @return DataFixtureLoader
+     */
+    public function loadPageContact(ObjectManager $manager)
+    {
+        $pageContact            = new Page();
+        $pageWidgetTypeContact  = new PageWidgetType($this->trans('Contact'));
 
         $pageContact
-            ->setTitle('Contact')
+            ->setTitle($this->trans('Contact'))
             ->setAbstract('')
-            ->setDescription('The contact page example.')
+            ->setDescription($this->trans('The contact page'))
         ;
 
-        $manager->persist($pageAbout);
         $manager->persist($pageContact);
+        $manager->persist($pageWidgetTypeContact);
 
         $pageWidget      = new PageWidget($pageContact, $pageWidgetTypeContact);
         $pageWidgetField = new PageWidgetField($pageWidgetTypeContact, 'Form Fields');
@@ -47,12 +59,64 @@ class DataFixtureLoader implements FixtureInterface
         $manager->persist($pageWidget);
         $manager->persist($pageWidgetField);
 
-        $customFields = array('Name', 'Email', 'Subject', 'Message');
+        $customFields = array(
+            $this->trans('Name'),
+            $this->trans('Email'),
+            $this->trans('Subject'),
+            $this->trans('Message'),
+            $this->trans('Submit'),
+        );
 
         foreach ($customFields as $value) {
             $widgetCustom = new PageWidgetCustomField($pageWidget, $pageWidgetField, $value);
             $manager->persist($widgetCustom);
         }
+
+        return $this;
+    }
+
+    /**
+     * Load Page About
+     *
+     * @param ObjectManager $manager
+     *
+     * @return DataFixtureLoader
+     */
+    public function loadPageAbout(ObjectManager $manager)
+    {
+        $pageAbout                  = new Page();
+        $pageWidgetTypeGoogleMap    = new PageWidgetType($this->trans('Google Map'));
+
+        $pageAbout
+            ->setTitle($this->trans('About'))
+            ->setAbstract('')
+            ->setDescription($this->trans('About our business'))
+        ;
+
+        $manager->persist($pageAbout);
+        $manager->persist($pageWidgetTypeGoogleMap);
+
+        $pageWidget = new PageWidget($pageAbout, $pageWidgetTypeGoogleMap);
+        $manager->persist($pageWidget);
+
+        $pageWidgetField = new PageWidgetField($pageWidgetTypeGoogleMap, 'Latitude');
+        $manager->persist($pageWidgetField);
+
+        $pageWidgetField = new PageWidgetField($pageWidgetTypeGoogleMap, 'Longitude');
+        $manager->persist($pageWidgetField);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this
+            ->loadPageContact($manager)
+            ->loadPageAbout($manager)
+        ;
 
         $manager->flush();
     }
